36df2a3cf316ec5c3d6e6abb2dcfba4653f24090
Filip <Filip@10.11.12.51>
2016-10-20 11:26:01 +0200

    Revert 1

--- src/test2/Test2.java
+++ src/test2/Test2.java
@@ -2,30 +2,6 @@ package test2;
 
 public class Test2 {
 
-    public void print() {
-        System.out.println("filip");
-        if ("test".equals("test")) {
-
-           
-        }
-        if (false) {
-
-            System.out.println("BATMAN");
-        }
-        
-
-   
-    }
-
-     public void printBeks()
-        {
-            System.out.println("Bekir Halvadzic");
-    }
-
-    public void filip() {
-        System.out.println("filip");
-    }
-
     public static void main(String[] args) {
         System.out.println("Johan är bäst av allt och alla!");
         System.out.println("Arsenal är inte bra!");
