3f5627846d778f387f443cb8f92c0dc9601722ae
Filip <Filip@10.11.12.51>
2016-10-20 11:32:21 +0200

    Reset

--- src/test2/Test2.java
+++ src/test2/Test2.java
@@ -3,26 +3,6 @@ package test2;
 public class Test2 {
 
     public static void main(String[] args) {
-        // Git 1
-        // Git 2
-        // Git 3
-        // Git 4 Github edit
-        // Git 5 Github edit 2
-        // Git 6 Netbeans
-        // Git 7 Netbeans
-        // Git 8 Netbeans
-        // Git 9 Netbeans
-        System.out.println("Johan är bäst av allt och alla!");
-        System.out.println("Arsenal är inte bra!");
-
-        System.out.println("Test1");
-    }
-
-    public void diego() {
-        System.out.println("Diego! Diego! Diego! Diego!");
-    }
-
-    public void tobias() {
-        System.out.println("Tobias");
+        
     }
 }
