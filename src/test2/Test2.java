4db626f2713bc089df852a27e606b971e57b47ad
Filip <Filip@10.11.12.51>
2016-10-20 10:25:20 +0200

    Merge origin/master
    Conflicts:
    	src/test2/Test2.java

--- src/test2/Test2.java
+++ src/test2/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("Johan är bäst av allt och alla!");
         System.out.println("Arsenal är riktigt dåliga!");
+        
+        System.out.println("Test1");
     }
     
 }
