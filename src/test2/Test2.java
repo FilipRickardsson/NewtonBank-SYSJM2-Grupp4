0ad35dd013230288042ac5b78648a97215c24927
Filip <Filip@10.11.12.51>
2016-10-20 10:42:50 +0200

    Added print method

--- src/test2/Test2.java
+++ src/test2/Test2.java
@@ -2,6 +2,10 @@ package test2;
 
 public class Test2 {
 
+    public void print() {
+        System.out.println("Print");
+    }
+    
     public static void main(String[] args) {
         System.out.println("Johan är bäst av allt och alla!");
         System.out.println("Arsenal är riktigt dåliga!");
