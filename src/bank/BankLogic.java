076e2b4ac0a251a5677993dbdd1b107a9745c502
Johan <Johan@10.11.12.111>
2016-10-24 12:06:16 +0200

    Merge origin/master
    Conflicts:
    	src/test2/Test2.java

--- src/bank/BankLogic.java
+++ src/bank/BankLogic.java
@@ -2,4 +2,21 @@ package bank;
 
 public class BankLogic {
     
+    public int addCreditAccount(long pNr){
+        int accountNumber = 1001;
+        
+        for (int i = 0; i<Customer.size(); i++)
+        {
+            if (pNr == customer.get[i].get(pNr)){
+        CreditAccount ca = new CreditAccount(pNR);
+        
+        accountNumber++;
+        break;
+            }
+            else
+                accountNumber = -1;
+                    }
+                return accountNumber;
+    }
+    public arrayList<transactions>getTransactions(long pNr, int accountID)
 }
