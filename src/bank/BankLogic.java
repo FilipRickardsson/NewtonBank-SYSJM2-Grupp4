ae05fae228cad09beb657b83c7ed01b80822e8cc
Filip <Filip@10.11.12.51>
2016-10-24 13:31:40 +0200

    Added method BranchA

--- src/bank/BankLogic.java
+++ src/bank/BankLogic.java
@@ -1,5 +1,51 @@
 package bank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BankLogic {
-    
+
+    List<Customer> costumers;
+    private int accountNbrCounter;
+
+    public BankLogic() {
+        costumers = new ArrayList<Customer>;
+        accountNbr = 1001;
+    }
+
+    public boolean changeCustomer(String name, long pNr) {
+        for (int i = 0; i < costumers.size(); i++) {
+            if (costumers.get(i).getPNr == pNr) {
+                costumers.get(i).setName(name);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // TODO Add saldo + interest
+    public List<String> removeCustomer(long pNr) {
+        List<String> info = new ArrayList();
+        for (int i = 0; i < costumers.size(); i++) {
+            if (costumers.get(i).getPNr == pNr) {
+                for (int j = 0; j < costumers.; j++) {
+                    costumer(i).get
+                }
+            }
+        }
+        
+    }
+
+    public int addSavingsAccount(long pNr) {
+        int currentAccountNbr = -1;
+        for (int i = 0; i < costumers.size(); i++) {
+            if (costumers.get(i).getPNr == pNr) {
+                costumers.get(i).getAccounts.add(new SavingsAccount(/*Insert inparameters here*/));
+                currentAccountNbr = accountNbrCounter;
+                accountNbrCounter++;
+            }
+        }
+        return currentAccountNbr;
+    }
+
 }
