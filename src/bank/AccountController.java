a36886151048f6961a8c39c2fd144cb57d3ed396
Filip <Filip@10.11.12.51>
2016-10-26 20:20:34 +0200

    Modified the popup, Initialize-method is usable again

--- src/bank/AccountController.java
+++ src/bank/AccountController.java
@@ -2,110 +2,86 @@ package bank;
 
 import java.awt.Button;
 import java.awt.TextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
 public class AccountController extends BaseController {
-    
 
     private BankLogic bankLogic;
-    
+
     private ObservableList<String> listan;
-    
+
     @FXML
     private TextField amount;
-    
+
     @FXML
     private ListView transactions;
-    
+
     @FXML
     private Button deposit;
-    
+
     @FXML
     private Button withdrawal;
-    
-    
-    @FXML
-    private void handleAmount(ActionEvent event){
-        
-        
-        
-    }
-    
-    @FXML
-    private void makeTransaction(ActionEvent event){
-        
-        
 
-            
-            //transaction.toString();
-
-        
-    }
-    
     @FXML
-    private void makeDeposit(ActionEvent event){
-        
+    private void handleAmount(ActionEvent event) {
 
-         //bankLogic.deposit(customer.getSsn(), transaction.getAccountID(), transaction.getAmount() );
-
-        
     }
-    
+
     @FXML
-    private void makeWithdrawal(ActionEvent event){
-        
+    private void makeTransaction(ActionEvent event) {
+        //transaction.toString();
     }
-    
+
+    @FXML
+    private void makeDeposit(ActionEvent event) {
+        //bankLogic.deposit(customer.getSsn(), transaction.getAccountID(), transaction.getAmount() );
+    }
+
+    @FXML
+    private void makeWithdrawal(ActionEvent event) {
+
+    }
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("Account Controller");
     }
-    
-    public void init(long ssn, int accountID){
-         
 
-         
+    public void init(long ssn, int accountID) {
         listan = FXCollections.observableArrayList(bankLogic.getTransactions(ssn, accountID));
-                
     }
-            
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-
-        bankLogic = BankLogic.getBankLogic();
-        
-        
-        
-    }    
 
     @Override
     protected void popupYes() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Yes");
+        popup.close();
     }
 
     @Override
     protected void popupNo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("No");
+        popup.close();
     }
 
     @Override
-    protected void init() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void initialize(URL url, ResourceBundle rb) {
+        bankLogic = BankLogic.getBankLogic();
+
+        try {
+            loadPopup();
+        } catch (IOException ex) {
+            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-
-
+    
 }
-
-
-
-
-
-   
--- src/bank/InfoController.java
+++ src/bank/InfoController.java
@@ -2,19 +2,18 @@ package bank;
 
 import java.awt.Label;
 import java.awt.TextField;
+import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
 public class InfoController extends BaseController {
-    
+
     private BankLogic bankLogic;
     private long Ssn;
     @FXML
@@ -29,44 +28,47 @@ public class InfoController extends BaseController {
     private Label balance;
     @FXML
     private ListView accountList;
-            
-    @FXML
-            private void nameSsnSet(){
-                // sett name and ssn textfields based on wich customer/account is closing
-        }
-            @FXML
-            private void balanceSet(){
-                //Set the balance textfield to total balance + interest for 
-//                closed accounts
-        
-}
-                
-    
-    ObservableList<String> accounts;
-    
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        BankLogic.getBankLogic();
-        accounts =FXCollections.observableArrayList(bankLogic.getAccount(9702020101L, 1001));
-        
-        accountList.setItems(accounts);
 
-    }    
+    private ObservableList<String> accounts;
+
+    @FXML
+    private void nameSsnSet() {
+        // sett name and ssn textfields based on wich customer/account is closing
+    }
+
+    @FXML
+    private void balanceSet() {
+        // Set the balance textfield to total balance + interest for 
+        // closed accounts
+    }
 
     @Override
     protected void popupYes() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Yes");
+        popup.close();
+
     }
 
     @Override
     protected void popupNo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("No");
+        popup.close();
+
     }
 
     @Override
-    protected void init() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void initialize(URL url, ResourceBundle rb) {
+        BankLogic.getBankLogic();
+        accounts = FXCollections.observableArrayList(bankLogic.getAccount(9702020101L, 1001));
+
+        accountList.setItems(accounts);
+
+        try {
+            loadPopup();
+        } catch (IOException ex) {
+            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
 }
