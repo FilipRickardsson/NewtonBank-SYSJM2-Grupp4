d588ae8f640447e860cf418b603fde29677ab206
Johan <Johan@10.11.12.111>
2016-10-27 10:48:08 +0200

    minor changes for testing info.FXML

--- src/bank/BankApplication.java
+++ src/bank/BankApplication.java
@@ -11,7 +11,7 @@ public class BankApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("Info.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
 
         Scene scene = new Scene(root);
 
