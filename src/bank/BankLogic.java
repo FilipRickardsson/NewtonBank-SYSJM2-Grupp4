ad2312e7fac1f746b6b2c48223c78fc09417f4d0
Christoffer <Christoffer@10.11.12.183>
2016-10-31 11:25:04 +0100

    Added method in bnklogic to check if a string contains numbers.
    EDIT: Removed. Did not work as intended

--- src/bank/BankLogic.java
+++ src/bank/BankLogic.java
@@ -357,18 +357,6 @@ public class BankLogic {
         return customer.getAccounts().get(AccountIdIndex).getAccountNumber();
     }
     
-    /**
-     * Checks if a string contains numbers.
-     * @param str
-     * @return 
-     */
-    public boolean isNumeric(String str) {
-        try {
-            double d = Double.parseDouble(str);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
-    }
+
 
 }
